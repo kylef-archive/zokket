51c349caa95b10c887edbce7625bc7eed417b71b
Kyle Fuller <inbox@kylefuller.co.uk>
2010-09-30 21:27:10 +0100

    ThreadedRunloop should be part of zokket module and not require additional importing

--- zokket/__init__.py
+++ zokket/__init__.py
@@ -1,3 +1,3 @@
 from zokket.sockets import SocketException, TCPSocket
 from zokket.timers import Timer
-from zokket.runloop import DefaultRunloop, Runloop
+from zokket.runloop import DefaultRunloop, Runloop, ThreadedRunloop
