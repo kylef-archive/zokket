1fa42e21d0b7a8b26a1508c329871ac8d9ad4bdd
Kyle Fuller <inbox@kylefuller.co.uk>
2014-08-23 15:05:01 +0100

    Introduce version 1.2.1

--- zokket/__init__.py
+++ zokket/__init__.py
@@ -4,10 +4,8 @@ from zokket.timers import Timer
 from zokket.runloop import DefaultRunloop, Runloop
 from zokket.wsgi import WSGIServer
 
-VERSION = (1, 2, 0)
+VERSION = (1, 2, 1)
 
 def get_version():
-    version = '{}.{}'.format(VERSION[0], VERSION[1])
-    if VERSION[2]:
-        version = '{}.{}'.format(version, VERSION[2])
-    return version
+    return '{}.{}.{}'.format(VERSION[0], VERSION[1], VERSION[2])
+
